1694727ff1e76af5673bbe2fbf37cac3113e1c03
Xianda Wang <wxianda@gmail.com>
2020-03-22 17:06:58 +0800

    Improve gallery style

--- plugins/photos_tag.rb
+++ plugins/photos_tag.rb
@@ -52,7 +52,7 @@ module Jekyll
     def path_for(filename)
       filename = filename.strip
       prefix = (@context.environments.first['site']['photos_prefix'] unless filename =~ /^(?:\/|http)/i) || ""
-      "#{prefix}#{filename}"
+      "#{prefix}#{filename}?size=1080"
     end
 
     def thumb_for(filename, thumb=nil)
@@ -62,7 +62,7 @@ module Jekyll
       #  thumb = (thumb unless thumb == 'default') || filename.gsub(/(?:_b)?\.(?<ext>[^\.]+)$/, "_m.\\k<ext>")
       #else
       #  thumb = (thumb unless thumb == 'default') || "#{filename}_m"
-      "#{path_for((thumb unless thumb == 'default') || filename)}?size=720"
+      "#{path_for((thumb unless thumb == 'default') || filename).gsub(/\?.+$/, '')}?size=720"
     end
   end
 
